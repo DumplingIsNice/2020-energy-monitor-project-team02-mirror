3dc10afd0dc1e8c9b482b610dace506a9ba12d99
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2020-08-21 22:07:46 +1200

    Update common.h

--- Firmware/common.h
+++ Firmware/common.h
@@ -1,4 +1,14 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+// Macros for important values.
+
+#define F_CPU 800000UL //Defining system clock frequency
+#define UDRR 4 // Prescaler value for initializing baud rate.
+// #define BAUD_RATE 9600UL
+
+// Libraries to include.
+
+#include <avr/io.h> //Needed for using the macros for register addresses
+
 #endif /* COMMON_H */
