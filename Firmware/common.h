24129d49939457988fd7b61d82a5d9c0519fbebd
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2020-08-21 22:58:02 +1200

    Moved macros from common.h to uart.h

--- Firmware/common.h
+++ Firmware/common.h
@@ -4,8 +4,6 @@
 // Macros for important values.
 
 #define F_CPU 800000UL //Defining system clock frequency
-#define UDRR 4 // Prescaler value for initializing baud rate.
-// #define BAUD_RATE 9600UL
 
 // Libraries to include.
 
--- Firmware/uart.h
+++ Firmware/uart.h
@@ -10,6 +10,11 @@
 #ifndef UART_H
 #define UART_H
 
+// Macros
+
+#define UART_BPS 9600UL // Baud rate / Bits Per Second value the UART module is configured to (_9600 BPS_)
+#define UBRR 4 // Prescaler value for initializing baud rate.
+
 void usart_init(uint32_t ubrr); // Initializes USART registers by specification and ubrr value. Author: Hao Lin (21/08/2020)
 
 #endif /* UART_H */
