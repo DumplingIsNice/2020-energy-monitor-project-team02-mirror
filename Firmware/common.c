5e8b739b68eea107bebc1ed96f345671a34aab8f
Ankush Patel <apat819@aucklanduni.ac.nz>
2020-08-24 18:29:37 +1200

    Added function to round floats

--- Firmware/common.c
+++ Firmware/common.c
@@ -10,3 +10,27 @@ static short num_length(int num)
 	while (num > 0) ++length, num /= 10;
 	return length;
 }
+
+/*
+ * Round the floating point number n to the number of decimal places specified.
+ * Note that decimalPlace = 0 means round to nearest int.
+ *
+ * Also note inaccuracies due to floating point representation
+ * 	e.g. fround(123.67) = 123.69999 not 123.7 as expected.
+ */
+float fround(float n, int decimalPlace)
+{
+	float f = 0;
+
+	switch (decimalPlace) {
+	case 0:
+		/* No rounding, just return int */
+		f = (n - (int) n > 0.5 ? (int) n + 1 : (int) n);
+		break;
+	default:
+		f = fround(n * decimalPlace * 10, 0) / decimalPlace * 10;
+		break;
+	}
+
+	return f;
+}
--- Firmware/uart.c
+++ Firmware/uart.c
@@ -14,82 +14,89 @@
 #define COMMA 0x2C
 #define ZERO 0x30
 
-void usart_init(uint32_t ubrr){ // UART init function. Author: Hao Lin (21/08/2020)
+static void ascii_convert(uint16_t *number)
+{ // Converts a unsigned number digit to ascii format. Author: Hao Lin (22/08/2020)
+	*number += 48;
+}
+
+static void extract_digits(uint16_t number, uint16_t *ones, uint16_t *tens, uint16_t *hundreds)
+{ // Extracts digits of a number below 1000. Author: Hao Lin (22/08/2020)
+	*ones = number % 10;
+	*tens = (number % 100 - *ones) / 10;
+	*hundreds = (number - *tens * 10 - *ones) / 100;
+}
+
+
+void usart_init(uint32_t ubrr)
+{ // UART init function. Author: Hao Lin (21/08/2020)
 
 	UDR0 = 0b00000000;
 	UCSR0A = 0b00000000;
 
-	UCSR0B |= (1<<TXEN0) & ~(1 << UCSZ02); // Set TXEN0 bit to 1 (enable transmit) and UCSZ02 bit to 0 (8 data bits)
+	UCSR0B |= (1 << TXEN0) & ~(1 << UCSZ02); // Set TXEN0 bit to 1 (enable transmit) and UCSZ02 bit to 0 (8 data bits)
 	UCSR0C |= (1 << UCSZ01) | (1 << UCSZ00); // UMSEL0[1..0]=00 (UART mode), UPM0[1..0]=00 (no parity), USBS0=0 (1 stop-bit), UCSZ0[1..0]=11 (8 data bits)
 	UBRR0 = ubrr;
 
 	// UBRR0 = ((F_CPU / (16*baud)) - 1); // Full eqn for ubrr value.
 }
 
-void usart_transmit(uint8_t data) { // UART character transmit function. Author: Krithik Lekinwala (21/08/2020)
-	while ((UCSR0A & (1<<UDRE0)) == 0) { // Continuously checking to see if UDRE0 bit is 0
+void usart_transmit(uint8_t data)
+{ // UART character transmit function. Author: Krithik Lekinwala (21/08/2020)
+	while ((UCSR0A & (1 << UDRE0)) == 0) { // Continuously checking to see if UDRE0 bit is 0
 		// If it is not 0, wait until it is so it starts writing the data to the register
-		
+
 	}
 	UDR0 = data; // Writing data to register
-	
+
 }
 
-void usart_transmitRaw(uint8_t rawData[], size_t arraySize) {
-	while ((UCSR0A & (1<<UDRE0)) == 0) { // Continuously checking to see if UDRE0 bit is 0
+void usart_transmitRaw(uint8_t rawData[], size_t arraySize)
+{
+	while ((UCSR0A & (1 << UDRE0)) == 0) { // Continuously checking to see if UDRE0 bit is 0
 		// If it is not 0, wait until it is so it starts writing the data to the register
-		
+
 	}
-	
+
 	//size_t totalBytes = sizeof(rawData)/sizeof(rawData[0]); // Returns size of the array input
-	for (int i = 0; i < arraySize; i++ ) {
+	for (int i = 0; i < arraySize; i++) {
 		UDR0 = rawData[i];
 	}
 	// UDR0 = data; // Writing data to register
-}
-
-static void ascii_convert(uint16_t *number){ // Converts a unsigned number digit to ascii format. Author: Hao Lin (22/08/2020)
-	*number += 48;
-}
-
-static void extract_digits(uint16_t number, uint16_t *ones, uint16_t *tens, uint16_t *hundreds){ // Extracts digits of a number below 1000. Author: Hao Lin (22/08/2020)
-	*ones = number%10;
-	*tens = (number%100 - *ones)/10;
-	*hundreds = (number - *tens*10 - *ones)/100;
 }
 
 // Print out an array of POSITIVE integers.
 // Inputs will be array of type int, and length of the array.
 // NOTE: the function should convert the numbers to ASCII before sending them over UART.
 // Author: Hao Lin (22/08/20)
-void print_array_intergers(uint16_t intArray[], uint16_t arrayLength){
-	
+void print_array_intergers(uint16_t intArray[], uint16_t arrayLength)
+{
+
 	uint16_t number = 0;
 	uint16_t ones = 0, tens = 0, hundreds = 0, i = 0;
-	
-	while (1){
-		
+
+	while (1) {
+
 		number = intArray[i];
-		
+
 		extract_digits(number, &ones, &tens, &hundreds);
-		
+
 		ascii_convert(&ones);
 		ascii_convert(&hundreds);
 		ascii_convert(&tens);
-		
-		if (hundreds != ZERO){
+
+		if (hundreds != ZERO) {
 			usart_transmit(hundreds);
 		}
-		
-		if (tens != ZERO){
+
+		if (tens != ZERO) {
 			usart_transmit(tens);
 		}
-		
+
 		usart_transmit(ones);
-		
+
 		i++;
-		
-		if (i >= arrayLength){
+
+		if (i >= arrayLength) {
 			break;
 		}
 		usart_transmit(COMMA);
