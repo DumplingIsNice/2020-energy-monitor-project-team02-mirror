666729e8b02ef494120da4915a65a158489096a8
rswe356 <68938353+rswe356@users.noreply.github.com>
2020-08-26 17:09:59 +1200

    Added terminal_print and fixed print_interger

--- Firmware/main.c
+++ Firmware/main.c
@@ -13,7 +13,21 @@ int main()
 #endif /* ENABLE_TESTING */
 
 	while (1) {
+		//terminal_print;
+		//_delay_ms(1000);
 	}
 
 	return 0;
 }
+
+
+
+void terminal_print(float rmsV, uint16_t current, float power){
+
+	print("RMS Voltage is: %d \n", rmsV);
+	
+	print("Peak Current is: %d \n", current);
+	
+	print("Power is: %d \n", power);
+}
+
