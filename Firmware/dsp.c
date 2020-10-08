5a4742414bf8fee0a0604c949e1e81f0890cd1fb
Ankush Patel <apat819@aucklanduni.ac.nz>
2020-10-08 19:01:33 +1300

    Added display modules in firmware

--- Firmware/dsp.c
+++ Firmware/dsp.c
@@ -1,7 +1,6 @@
 #include "common.h"
 #include "adc.h"
 
-/* TODO: THIS FUNCTION NEEDS TO BE IMPLIMENTED */
 /* Remove the gain and shifts added by all the analogue circuitry to get
  * back the original sensor voltage value
  */
