c0e354c1b3cc1e7d6af679f77a983f2b4187f563
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2020-10-08 21:59:34 +1300

    Implemented voltage_zc ISR. Period calc not yet implemented.

--- Firmware/dsp.h
+++ Firmware/dsp.h
@@ -1,5 +1,17 @@
 /* Remove the gain and shifts added by all the analogue circuitry to get
  * back the original sensor voltage value
  */
+
+#include <stdbool.h>
+
 float reverse_voltage_gain(float adc_voltage);
 float reverse_current_gain(float adc_voltage);
+
+/* Returns whether is sampling or not */
+bool is_sampling();
+
+/* Initializes voltage zero crossing interrupt */
+void voltage_zc_interrupt_init();
+
+/* Initializes current zero crossing interrupt (Not implemented) */
+// void current_zc_interrupt_init();
