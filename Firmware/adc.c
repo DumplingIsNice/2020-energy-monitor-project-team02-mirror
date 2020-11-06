4ef38910d079f867b90234cc3201b3de855e393e
uoa-hlin784 <hlin784@aucklanduni.ac.nz>
2020-11-06 14:41:17 +1300

    Dirty PCB push: Defined HARDWARE_BUILD. Removed gain = 0. SET REF0. Changed correct prescalar for adc.

--- Firmware/adc.c
+++ Firmware/adc.c
@@ -35,10 +35,13 @@ void adc_init()
 	 * To get the full 10-bit resolution , the ADC clock must be at a maximum of 200 KHz
 	 * Prescaler = f_cpu / 200000
 	 */
-	SET_PORT(ADCSRA, ADEN), SET_PORT(ADCSRA, ADSC), SET_PORT(ADCSRA, ADATE), SET_PORT(ADCSRA, ADIE);
+	SET_PORT(ADCSRA, ADEN), SET_PORT(ADCSRA, ADSC), SET_PORT(ADCSRA, ADATE), SET_PORT(ADCSRA, ADIE), SET_PORT(ADMUX, REFS0);
 #ifdef HARDWARE_BUILD
 	/* For hardware, prescaler must be 128 */
-	SET_PORT(ADCSRA, ADPS2), SET_PORT(ADCSRA, ADPS1), SET_PORT(ADCSRA, ADPS0);
+	// SET_PORT(ADCSRA, ADPS2), SET_PORT(ADCSRA, ADPS1), SET_PORT(ADCSRA, ADPS0);
+	
+	/* For hardware, prescaler must be 64 */
+	SET_PORT(ADCSRA, ADPS2), SET_PORT(ADCSRA, ADPS1), CLR_PORT(ADCSRA, ADPS0);
 #else
 	/* For simulation, prescaler can be 4 */
 	SET_PORT(ADCSRA, ADPS1);
@@ -53,8 +56,10 @@ ISR(ADC_vect)
 	/* Occurs every 1 ms (uncomment LED toggle code below to test) */
 	//TGL_PORT(PORTB, PORTB5);
 	if (current_adc_channel == ADC_CH_VOLTAGE) {
+		//print("%f\r\n", (float)ADC);
 		adc_voltages[adc_voltages_head++] = ADC;
 	} else {
+		//print("%u\r\n", ADC);
 		adc_currents[adc_currents_head++] = ADC;
 	}
 }
--- Firmware/dsp.c
+++ Firmware/dsp.c
@@ -188,10 +188,6 @@ void adc2real_voltage()
 	const uint16_t R1 = 4700;
 	float amplifierGain =  1 / (float) (R2 / ((float) R1));
 
-#ifdef  HARDWARE_BUILD
-	/* The XplainedMini does not have resistors/opamps/gains etc. */
-	dividerGain = amplifierGain = 0;
-#endif /* HARDWARE_BUILD */
 	for (i = 0; i < RAW_ARRAY_SIZE; ++i) {
 		/* The voltage values in raw_currents are actually the ADC values
 		 * Overwrite them with the actual raw voltage values
@@ -218,11 +214,6 @@ void adc2real_current()
 	const uint16_t R1 = 22000;
 	float amplifierGain =  1 / (float) (R2 / ((float) R1));
 	
-#ifdef  HARDWARE_BUILD
-	/* The XplainedMini does not have resistors/opamps/gains etc. */
-	dividerGain = amplifierGain = 0;
-#endif /* HARDWARE_BUILD */
-	
 	for (i = 0; i < RAW_ARRAY_SIZE; ++i) {
 		/* The voltage values in raw_currents are actually the ADC values
 		 * Overwrite them with the actual raw voltage values
@@ -236,7 +227,7 @@ ISR(INT0_vect)
 {
 	extern volatile unsigned enable_zc;
 	//Use this LED to check if interrupt is called.
-	//TGL_PORT(PORTB, PORTB5);
+	TGL_PORT(PORTB, PORTB5);
 	
 	/* Zero crossing indicates the start or end of a cycle of sampling */
 	if (!currently_sampling && enable_zc) {
